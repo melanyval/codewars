6eb40a0636125e692d0469f7857bfc850408f84e
Melany Valdiviezo <melanyvaldiviezo@gmail.com>
2019-12-18 21:30:00 -0500

    init

---
+++ index.js
@@ -0,0 +1,20 @@
+// Problem 1:
+
+// Reverse this string so it says Tacos backwards: "socaT"
+
+let word = "Tacos"
+
+function reverseString(str){
+  return str.split("").reverse().join("");
+}
+
+reverseString(word);
+
+// QUESTION 2:
+
+// write a function that reverses the word order of a sentence
+
+// example: "cuban sandwiches" would become "sandwiches cuban"
+
+let delicious = "cuban sandwiches"
+

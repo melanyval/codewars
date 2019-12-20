d87bef74e666f618206692eee57aeb14942f3b9c
Melany Valdiviezo <melanyvaldiviezo@gmail.com>
2019-12-20 00:31:05 -0500

    upd

--- index.js
+++ index.js
@@ -27,3 +27,36 @@ function reverseSentence(str){
 
 reverseSentence(truth);
 
+
+// Question 3:
+
+// Given a menu of delicious food, write a function that takes 2 arguments:
+
+// 1. The number of food items you will order. `num`
+
+// 2. The menu of food items that it will select from. `menu`
+
+// Your function should return an array of random food items from the `menu` of length `num`
+
+let tasty = [
+  'burger',
+  'pizza',
+  'overpriced salad', 
+  'not cucumbers', 
+  'candy that is 99% sugar',
+  'tequenos', 
+  'the top part of a muffin', 
+  'any kind of meat on any kind of rice', 
+  'all cuban food tbh'
+]
+
+function randomFood(num, menu) {
+  let finalArr = []
+  for (let i = 0; i <= num; i++){
+  let randomFood = menu[Math.floor(Math.random()*menu.length)]
+  finalArr.push(randomFood);
+  }
+  return finalArr
+}
+
+randomFood(5, tasty)

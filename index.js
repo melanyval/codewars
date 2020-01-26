d28037aa692cdb56c3fab37a761911addc5f673b
Melany Valdiviezo <melanyvaldiviezo@gmail.com>
2020-01-26 15:18:43 -0500

    hello

--- index.js
+++ index.js
@@ -164,8 +164,9 @@ function oddOrEven(array) {
     return ("odd")
   }
 }
+//////////////////////////////////////////////////////////////////////////////
 
-// question 8
+// Question 8
 // https://www.codewars.com/kata/539a0e4d85e3425cb0000a88/train/javascript
 
 // function sum(){

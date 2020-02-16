77e1a246b99a01bf97f2f39c572ca4d326e8c353
Melany Valdiviezo <melanyvaldiviezo@gmail.com>
2020-02-16 00:00:16 -0500

    kata

--- index.js
+++ index.js
@@ -163,4 +163,33 @@ function oddOrEven(array) {
   }
 
   // question 8
-  ////
+  // https://www.codewars.com/kata/539a0e4d85e3425cb0000a88/train/javascript
+
+// function sum(){
+// 	var sum = 0;
+// 	return function addition(num){
+// 		return sum += num;
+// 	}
+// }
+// var add = sum();
+// add(2)
+// add(3);
+
+// We want to create a function that will add numbers together when called in succession.
+
+// add(1)(2);
+// // returns 3
+
+function add(n){
+  var fn = function(x) {
+    return add(n + x);
+  };
+  
+  fn.valueOf = function() {
+    return n;
+  };
+  
+  return fn;
+}
+
+add(1);

fb3061a59f70c26c3120d4aa3d90d3c76ee64b02
Melany Valdiviezo <melanyvaldiviezo@gmail.com>
2020-02-16 19:45:35 -0500

    newKata

--- index.js
+++ index.js
@@ -193,3 +193,24 @@ function add(n){
 }
 
 add(1);
+
+// Question 9
+// Description:
+// ROT13 is a simple letter substitution cipher that replaces a letter with the letter 13 letters after it in the alphabet. ROT13 is an example of the Caesar cipher.
+
+// Create a function that takes a string and returns the string ciphered with Rot13. If there are numbers or special characters included in the string, they should be returned as they are. Only letters from the latin/english alphabet should be shifted, like in the original Rot13 "implementation".
+
+function rot13(str) {
+  var input     = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  var output    = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm';
+  var index     = x => input.indexOf(x);
+  var translate = x => index(x) > -1 ? output[index(x)] : x;
+  return str.split('').map(translate).join('');
+}
+
+/////////////////////////////////////////////////////////////////////////////
+
+// Question 10
+
+//
+

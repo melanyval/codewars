a7e2677ccbdfadf18a2158608a19587b36c0ae6e
Melany Valdiviezo <melanyvaldiviezo@gmail.com>
2020-02-19 13:20:43 -0500

    kat4

--- index.js
+++ index.js
@@ -196,8 +196,9 @@ function add(n) {
 }
 
 add(1);
-
+/////////////////////////////////////////////////////////////////////////////
 // Question 9
+
 // Description:
 // ROT13 is a simple letter substitution cipher that replaces a letter with the letter 13 letters after it in the alphabet. ROT13 is an example of the Caesar cipher.
 

b7ddb2e87b60abdb3c2844f086d3a1f2357a56cd
Melany Valdiviezo <melanyvaldiviezo@gmail.com>
2020-02-28 15:20:43 -0500

    details

--- index.js
+++ index.js
@@ -198,6 +198,8 @@ function add(n) {
 }
 
 add(1);
+
+
 /////////////////////////////////////////////////////////////////////////////
 // Question 9
 
@@ -215,7 +217,6 @@ function rot13(str) {
 }
 
 /////////////////////////////////////////////////////////////////////////////
-
 // Question 10
 
 //
@@ -275,3 +276,58 @@ function findMissingLetter(array)
 }
 
 /////////////////////////////////////////////////////////////////////////////
+
+// Question 11
+
+//Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+
+// For example:
+// persistence(39) === 3 // because 3*9 = 27, 2*7 = 14, 1*4= 4 and 4 has only one digit
+// persistence(999) === 4 // because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2
+// persistence(4) === 0 // because 4 is already a one-digit number
+// */
+
+function persistence(num) {
+ //set multiplyCount equal 0
+ //turn the number into an array of nums
+ //while the length of the array is more than 1
+   //multiply each digit together
+   //the product becomes the new arr
+ //return multiplyCount
+
+ var arr = num.toString().split("");
+ var multiplyCount = 0;
+ var product;
+ while (arr.length > 1) {
+   product = arr.reduce(function(a, b) {
+     return a * b;
+   });
+   multiplyCount++;
+   arr = product.toString().split("");
+ }
+ return multiplyCount;
+}
+
+persistence(39); // 3
+persistence(999); // 4
+persistence(4); // 0
+
+
+//other solution=========================================
+function persistenceB(num) {
+ var times = 0;
+ num = num.toString();
+ while (num.length > 1) {
+   times++;
+   num = num.split('').map(Number).reduce((a, b) => a * b).toString();
+ }
+ return times;
+}
+
+/////////////////////////////////////////////////////////////////////////
+
+// Question 12
+
+//
+
+
